63412a7e93f60076b95de55cd65c4cfa99ed3ede
jonahplumb <98274184+jonahplumb@users.noreply.github.com>
2022-04-26 11:37:21 -0400

    Update Valorant-Smurf-Script.py

--- Valorant-Smurf-Script.py
+++ Valorant-Smurf-Script.py
@@ -2,8 +2,7 @@ import pyautogui
 import time
 
 time.sleep(3)
-#Following commands are opening our windows search bar, typing Valorant and pressing enter
-#This will gurantee it works, no matter where you have Valorant downloaded
+# Opening our windows search bar, typing Valorant and pressing enter
 pyautogui.hotkey('winleft')
 pyautogui.typewrite('Valorant\n', 0.5)
 # This pause could be shorter but dependent on PC hardware the riot client can 
@@ -12,11 +11,11 @@ time.sleep(8)
 # Delete the #Username & #Password and enter your Username and Password,
 pyautogui.typewrite(#Username\t") #Entering our username 
 pyautogui.typewrite(#Password\n") #Entering our password
-# Sometimes to right client requires players to press "Play"
+# Sometimes to riot client requires players to press "Play"
 # Dependent on 1920x1080
 time.sleep(2)
 pyautogui.click(x=372,y=911)
-# This is a breif pause to allow Valorant to load, then clicking in the lower right
+# Allow Valorant to load, then clicking in the lower right
 # of our screen to avoid the mouse-glitch which has been a bug in Valorant for some time
 time.sleep(8)
 pyautogui.click(x=1887,y=809)
