370aa453d7c3f9892c1d0cbdae1031b8492e758a
jonahplumb <98274184+jonahplumb@users.noreply.github.com>
2022-05-05 13:55:15 -0400

    Add files via upload

---
+++ Daily-Tasks.ps1
@@ -0,0 +1,5 @@
+﻿Start-Process "https://outlook.office.com/mail/" # Opens USF email inbox
+Start-Process "https://mail.google.com/mail/u/1/?ogbl#inbox" # Opens gmail inbox
+Start-Process "https://usflearn.instructure.com/" # Opens canvas
+Start-Process "https://youtube.com"
+Start-Process "C:\Users\Plumb\AppData\Local\Discord\app-1.0.9004\Discord.exe" # Opens discord
