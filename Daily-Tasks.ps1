2e1e0abf2f60fb9e2c29dc697f686d9621e538c7
jonahplumb <98274184+jonahplumb@users.noreply.github.com>
2022-05-05 13:56:10 -0400

    Update Daily-Tasks.ps1

--- Daily-Tasks.ps1
+++ Daily-Tasks.ps1
@@ -1,5 +1,5 @@
 ﻿Start-Process "https://outlook.office.com/mail/" # Opens USF email inbox
-Start-Process "https://mail.google.com/mail/u/1/?ogbl#inbox" # Opens gmail inbox
+Start-Process "https://mail.google.com/mail/u/1/********#inbox" # Opens gmail inbox
 Start-Process "https://usflearn.instructure.com/" # Opens canvas
 Start-Process "https://youtube.com"
-Start-Process "C:\Users\Plumb\AppData\Local\Discord\app-1.0.9004\Discord.exe" # Opens discord
+Start-Process "C:\Users\*****\AppData\Local\Discord\app-1.0.9004\Discord.exe" # Opens discord
