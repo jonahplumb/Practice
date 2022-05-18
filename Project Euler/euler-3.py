faa2337861407d868f3e194cc8cdc29a22359042
jonahplumb <98274184+jonahplumb@users.noreply.github.com>
2022-05-18 19:04:36 -0400

    Add files via upload

---
+++ Project Euler/euler-3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import math
+
+num = 600851475143
+largestPrime = 0
+i = 2
+while i * i < num:
+    while num % i == 0:
+        num /= i
+    i += 1
+    
+print(int(num))
+
