ca004f1dbc242a773593963e8bb38ec6a7b30b31
jonahplumb <98274184+jonahplumb@users.noreply.github.com>
2022-10-30 13:58:11 -0400

    Create hello-world.go

---
+++ GO-Practice/hello-world.go
@@ -0,0 +1,14 @@
+// First GO program following documentaion module below
+//  https://go.dev/doc/tutorial/getting-started
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	// fmt.Print("Hello World, first go program")
+	fmt.Println(quote.Go())
+}
