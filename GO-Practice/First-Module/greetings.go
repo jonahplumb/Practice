7566096a25a11b7f43098c2e86ba7dca1f4ed52d
jonahplumb <98274184+jonahplumb@users.noreply.github.com>
2022-11-02 20:16:26 -0400

    Add files via upload

---
+++ GO-Practice/First-Module/greetings.go
@@ -0,0 +1,16 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("Jonah Plumb")
+	}
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
+}
