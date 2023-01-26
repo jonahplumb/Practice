4f9670916456e0bf498b0e009d842ec1002705b3
jonahplumb <98274184+jonahplumb@users.noreply.github.com>
2023-01-25 19:44:22 -0500

    Delete Daily-Tasks.ps1

--- Daily-Tasks.ps1
+++
@@ -1,5 +0,0 @@
-﻿Start-Process "https://outlook.office.com/mail/" # Opens USF email inbox
-Start-Process "https://mail.google.com/mail/u/1/********#inbox" # Opens gmail inbox
-Start-Process "https://usflearn.instructure.com/" # Opens canvas
-Start-Process "https://youtube.com"
-Start-Process "C:\Users\*****\AppData\Local\Discord\app-1.0.9004\Discord.exe" # Opens discord
