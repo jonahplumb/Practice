80582bc4a0dd3f7280fc9950e570a08efaaba166
Jonah <plumbjonah@gmail.com>
2024-03-02 21:57:39 -0500

    Practice for project matus

--- rabbitmq-testing/sender.go
+++ rabbitmq-testing/sender.go
@@ -2,18 +2,76 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+const (
+	charset    = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	charsetLen = len(charset)
+)
+
 type HelloData struct {
-	User string `json:"Hello"`
-	Boss string `json:"Bye"`
+	TechName   string `json:"Technology-Name"`
+	Action     string `json:"Action-Name"`
+	ActionId   string `json:"Action-Id"`
+	DatabaseId string `json:"Database-Id"`
 }
+
+// / Method to generate random id for our actions
+// / This will be the id used in lookup calls in my /action call
+func GenerateRandomID() string {
+	// Set the seed for the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Initialize a byte slice to store the random characters
+	idBytes := make([]byte, 19) // 4 * 4 characters + 3 hyphens
+
+	// Generate random characters and hyphens directly in the byte slice
+	for i := range idBytes {
+		// Add hyphen after each segment except the last one
+		if i%5 == 4 && i != 18 {
+			idBytes[i] = '-'
+		} else {
+			// Generate a random index within the charset
+			randIndex := rand.Intn(charsetLen)
+			// Assign the random character to the byte slice
+			idBytes[i] = charset[randIndex]
+		}
+	}
+
+	// Return the byte slice as a string
+	return string(idBytes)
+}
+
+// / Method to generate random hash for our database
+func GenerateSHA256Hash(input string) string {
+	// Convert the string to bytes
+	data := []byte(input)
+
+	// Initialize the SHA-256 hash algorithm
+	hasher := sha256.New()
+
+	// Write the data to the hasher
+	hasher.Write(data)
+
+	// Calculate the SHA-256 hash
+	hashBytes := hasher.Sum(nil)
+
+	// Convert the hash bytes to a hexadecimal string
+	hashString := hex.EncodeToString(hashBytes)
+
+	return hashString
+}
+
+//////////// End of Support Methods
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -22,9 +80,15 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
+	randomID := GenerateRandomID()
+	input := "Hello, world!"
+	hash := GenerateSHA256Hash(input)
+	fmt.Println(hash)
 	helloData := HelloData{
-		User: "World",
-		Boss: "World :(",
+		Action:     "get-user",
+		TechName:   "entra-id",
+		ActionId:   randomID,
+		DatabaseId: hash, // Just for testing, wont have this value in the real code
 	}
 
 	jsonData, err := json.Marshal(helloData)
@@ -53,7 +117,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	body := "Hello World!"
+	body := fmt.Sprintf("Hello World! - %s", randomID)
 	err = ch.PublishWithContext(ctx,
 		"",     // exchange
 		q.Name, // routing key
