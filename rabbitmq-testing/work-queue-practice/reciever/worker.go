c13fe78ecdc9f405bb1c87e3e3d0e9bd6c8574c9
Jonah <plumbjonah@gmail.com>
2024-03-11 21:37:04 -0400

    This looks wrong but changing file

--- rabbitmq-testing/work-queue-practice/reciever/worker.go
+++ rabbitmq-testing/work-queue-practice/reciever/worker.go
@@ -52,7 +52,6 @@ func main() {
 	failOnError(err, "Failed to register a consumer")
 
 	var forever chan struct{}
-
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
